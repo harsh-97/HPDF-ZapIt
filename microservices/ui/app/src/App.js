87f504437f964741fa27ae855eb006a39fef2f3d
harsh-97 <sethharsh.sureshchand2016@vitstudent.ac.in>
2018-01-30 23:19:54 +0530

    Added React Router and Material Ui modules to the ReactJS, Now routing through React Router

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Mui from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'; 
+
 import logo from './logo.svg';
 import './App.css';
+
+const muiTheme = getMuiTheme({
+    fontFamily: 'Arial, sans-serif',
+});
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Mui muiTheme={muiTheme}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Mui>
     );
   }
 }
 
-export default App;
+
+const Redirect = () => (
+  <Switch>
+    <Route exact path="/" component={App}/>
+  </Switch>
+);
+
+
+export default Redirect;
