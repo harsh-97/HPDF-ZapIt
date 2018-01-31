9e200a3b568293db272e1c195d760e75b957633a
harsh-97 <sethharsh.sureshchand2016@vitstudent.ac.in>
2018-01-31 14:18:45 +0530

    Further Experiments with React Router [2]

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -22,7 +22,8 @@ class App extends Component {
             <h1 className="App-title">Welcome to React</h1>
           </header>
           <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
+            To get started, edit <code>src/App.js</code> and save to reload.<br/>
+            <code>This should reflect after the push</code>
           </p>
         </div>
       // </Mui>
