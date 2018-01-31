712a61f04613ccc5f42c519200e6f218061772cd
harsh-97 <sethharsh.sureshchand2016@vitstudent.ac.in>
2018-01-31 15:09:38 +0530

    Added Material-UI support

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-// import Mui from 'material-ui/styles/MuiThemeProvider';
-// import getMuiTheme from 'material-ui/styles/getMuiTheme'; 
+import Mui from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'; 
+import {blue500} from 'material-ui/styles/colors';
+
+import IconButton from 'material-ui/IconButton';
+
+import AtomIcon from 'material-ui/svg-icons/hardware/toys';
+
 
 import logo from './logo.svg';
 import './App.css';
 
-// const muiTheme = getMuiTheme({
-//     fontFamily: 'Arial, sans-serif',
-// });
+const muiTheme = getMuiTheme({
+		palette: {
+			primary1Color: blue500,
+		},
+	    fontFamily: 'Arial, sans-serif',
+});
 
 
 class App extends Component {
   render() {
     return (
-      // <Mui muiTheme={muiTheme}>
+      <Mui muiTheme={muiTheme}>
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -25,8 +34,11 @@ class App extends Component {
             To get started, edit <code>src/App.js</code> and save to reload.<br/>
             <code>"This should reflect after the push"</code>
           </p>
+      		<IconButton tooltip="Go to Test Route" iconStyle={{width: 30, height: 30}} style={{width: 30, height: 30, padding: 0}} href="/testroute">
+				<AtomIcon color={muiTheme.palette.primary1Color}/>
+			</IconButton>
         </div>
-      // </Mui>
+      </Mui>
     );
   }
 }
@@ -34,9 +46,14 @@ class App extends Component {
 
 function Test(props){
   return(
-    <h4>
-    Test Route Successful!
-    </h4>
+  	<Mui muiTheme={muiTheme}>
+	    <h4>
+    	Test Route Successful!
+    	</h4>
+    	<IconButton tooltip="Go to Index" iconStyle={{width: 30, height: 30}} style={{width: 30, height: 30, padding: 0, marginLeft: '4%'}} href="/">
+				<AtomIcon color={muiTheme.palette.primary1Color}/>
+		</IconButton>
+	</Mui>
     );
 }
 
