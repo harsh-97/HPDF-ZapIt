52b6979595a6aa3598f3212356bb097fc3c17d4c
gauri-97 <gauri.singh2016@vitstudent.ac.in>
2018-02-02 22:17:18 +0530

    Login

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,5 +1,8 @@
 from src import app
 from flask import jsonify
+import requests
+from flask import request
+
 
 
 @app.route("/")
@@ -11,3 +14,15 @@ def home():
 @app.route("/json")
 def json_message():
     return jsonify(message="Hello World")
+
+#Login
+
+@app.route('/login',methods=['GET'])
+def login():
+	#username=request.form[username]
+	#password=request.form[password]
+	res=requests.post(auth.cramping38.hasura-app.io/login,{'username':username,'password':password})	
+	res=res.json()
+	print(res.text)
+	return "Login-done?"	
+
