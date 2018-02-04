aee65e6a7ddf12c05f8bac7c16b30da7edcdb850
gauri-97 <gauri.singh2016@vitstudent.ac.in>
2018-02-04 17:37:23 +0530

    added user-tables

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,6 +1,7 @@
 from src import app
 from flask import jsonify
 import requests
+import json
 from flask import request
 
 
@@ -27,4 +28,30 @@ def login():
 	res=res.json()
 	print(res.text) '''
 	return jsonify({'username': username,'password': password})	
+@app.route('/user-tables',methods=['GET'])
+def userTables():
+	url = "https://data.cramping38.hasura-app.io/v1/query"
+	user_id=2
+	requestPayload = {
+	    "type": "select",
+	    "args": {
+	        "table": "User_tables",
+	        "columns": [
+	            "table_id"
+	        ],
+	        "where": {
+	            "user_id": {
+	                "$eq": "user_id"
+	            }
+	        }
+	    }
+	}
+	headers = {
+    "Content-Type": "application/json"
+	}
 
+	# Make the query and store response in resp
+	resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+	# resp.content contains the json response.
+	print(resp.content)
